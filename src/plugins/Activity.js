c2401321f25727edc258d5fff09ef53667fbfeeb
Pandaaa2507 <Pandaaa2507@users.noreply.github.com>
2020-06-04 15:13:48 +0200

    Update 1.1.4 (FIX 01)

--- src/plugins/Activity.js
+++ src/plugins/Activity.js
@@ -1,11 +1,10 @@
 const { client } = require('../shard');
-const mainDir = require('../../mainDir');
 const fs = require('fs');
 
 module.exports = {
     name: 'Activity',
     enable() {
-        let package = JSON.parse(fs.readFileSync(`${mainDir}/package.json`, 'utf8'));
+        let package = require('../../package.json');
         client.user.setActivity(`Trap Cosmos | v${package.version}`, {
             'type': 'WATCHING'
         });
