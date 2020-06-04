7ada1cbe4e0c7777aa0b8e6436f2b366f11dba54
Pandaaa2507 <Flo.h_Panda@gmx.at>
2020-06-04 16:09:10 +0200

    Update 1.1.4 (FIX 08)

--- src/config.js
+++ src/config.js
@@ -5,6 +5,11 @@ module.exports = {
     token: process.env.TOKEN,
     shards: 'auto',
     youtube_api_token: process.env.YOUTUBE_API_TOKEN,
+    youtube_uploads: {
+        guild: '472885726275436556',
+        channel: '472885726984536065',
+        role: '718069501119168603'
+    },
     plugins: [
         'Activity',
         'Commands',
--- src/plugins/YouTubeUploads/index.js
+++ src/plugins/YouTubeUploads/index.js
@@ -82,9 +82,9 @@ module.exports = {
 
                     if (video.snippet.publishedAt !== latestVideo) {
 
-                        let guild = client.guilds.get('472885726275436556');
-                        let channel = guild.channels.get('472885726984536065');
-                        let role = guild.roles.get('718069501119168603');
+                        let guild = client.guilds.get(config.youtube_uploads.guild);
+                        let channel = guild.channels.get(config.youtube_uploads.channel);
+                        let role = guild.roles.get(config.youtube_uploads.role);
 
                         if(channel) {
                             channel.send(role, newVidEmbed);
